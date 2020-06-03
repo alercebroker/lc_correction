9128b9b23ce91158d72859bb4a49fa125c0efd06
Luis Sabatini <lsabatini@Luiss-MacBook-Pro.local>
2020-06-03 12:46:46 -0400

    test vscode -> git

--- correction/compute.py
+++ correction/compute.py
@@ -6,7 +6,7 @@ from astropy.time import Time
 #level = logging.INFO
 #logging.basicConfig(level=level,
 #                    format='%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s',
-#                    datefmt='%Y-%m-%d %H:%M:%S',) test
+#                    datefmt='%Y-%m-%d %H:%M:%S',)
 
 
 def validate_object():

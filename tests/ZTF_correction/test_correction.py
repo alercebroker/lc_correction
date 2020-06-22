84a5665c5c0f80a2a7453a2f6104e7372760b610
JavierArredondo <javier.arredondo.c@usach.cl>
2020-06-22 15:16:11 -0400

    ADD: Uses case notebook and testing

--- tests/ZTF_correction/test_correction.py
+++ tests/ZTF_correction/test_correction.py
@@ -7,6 +7,7 @@ from correction.compute import *
 
 AVRO_PATH = "data_examples/avros"
 CSV_PATH = "data_examples/csv"
+PARQUET_PATH = "data_examples/parquets"
 
 
 def read_avro(avro_path):
@@ -81,3 +82,57 @@ class TestDataframeCorrection(unittest.TestCase):
         self.assertEqual(len(dubious), 2)
         self.assertTrue(example["corrected"])
         self.assertTrue(example["dubious"])
+
+
+class TestDataframeCorrectionChain(unittest.TestCase):
+    def setUp(self) -> None:
+        self.detections = pd.read_parquet(os.path.join(PARQUET_PATH, "10_objects_detections.parquet"))
+        self.non_detections = pd.read_parquet(os.path.join(PARQUET_PATH, "10_objects_non_detections.parquet"))
+        self.corrected = pd.read_parquet(os.path.join(PARQUET_PATH, "10_objects_corrected.parquet"))
+        self.magstats = pd.read_parquet(os.path.join(PARQUET_PATH, "10_objects_magstats.parquet"))
+        self.unique_objects = 10
+        self.corrected_cols = ["corrected", "magpsf_corr", "sigmapsf_corr", "sigmapsf_corr_ext", "dubious"]
+        self.magstats_cols = ['corrected', 'nearZTF', 'nearPS1', 'stellarZTF', 'stellarPS1', 'stellar', 'ndet',
+                              'ndubious', 'nrfid', 'magpsf_mean', 'magpsf_median', 'magpsf_max', 'magpsf_min',
+                              'magpsf_first', 'sigmapsf_first', 'magpsf_last', 'magpsf_corr_mean', 'magpsf_corr_median',
+                              'magpsf_corr_max', 'magpsf_corr_min', 'magpsf_corr_first', 'magpsf_corr_last', 'magap_mean',
+                              'magap_median', 'magap_max', 'magap_min', 'magap_first', 'magap_last', 'first_mjd', 'last_mjd']
+        self.objstats_cols = ['nearZTF', 'nearPS1', 'stellar', 'corrected', 'ndet', 'ndubious']
+        self.detections["mjd"] = self.detections.jd - 2400000.5
+        del self.detections["jd"]
+        self.non_detections["mjd"] = self.non_detections.jd - 2400000.5
+        del self.non_detections["jd"]
+
+    def test_check_dataset(self):
+        unique_detections = len(self.detections.objectId.unique())
+        unique_non_detections = len(self.non_detections.objectId.unique())
+        self.assertEqual(unique_detections, self.unique_objects)
+        self.assertEqual(unique_non_detections, self.unique_objects)
+
+    def test_apply_correction_df(self):
+        corrected = self.detections.groupby(["objectId", "fid"]).apply(apply_correction_df)
+        for col in self.corrected_cols:
+            self.assertIn(col, corrected.columns)
+        self.assertEqual(len(corrected.index.levels[0]), self.unique_objects)
+        self.assertEqual(corrected.corrected.values.sum(), 1373)
+
+    def test_apply_mag_stats(self):
+        magstats = self.corrected.groupby(["objectId", "fid"]).apply(apply_mag_stats)
+        self.assertEqual(len(magstats.index.levels[0]), 10)
+        self.assertEqual(len(magstats), 16)
+        for col in self.magstats_cols:
+            self.assertIn(col, magstats.columns)
+
+    def test_apply_object_stats(self):
+        objstats = self.magstats.groupby(["objectId", "fid"]).apply(apply_object_stats)
+        self.assertEqual(len(objstats.index.levels[0]), 10)
+        self.assertEqual(len(objstats), 16)
+        for col in self.objstats_cols:
+            self.assertIn(col, objstats.columns)
+        self.assertEqual(objstats.ndubious.sum(), 4)
+
+    def test_apply_do_dmdt_df(self):
+        dmdt = do_dmdt_df(self.magstats, self.non_detections)
+        self.assertEqual(len(dmdt.index.levels[0]), 10)
+        self.assertEqual(len(dmdt), 16)
+        self.assertEqual(dmdt.close_nondet.sum(), 1)
